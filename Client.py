c7d5c51144657de3b9f1d03644997cbcab6a8d5b
PsSarvna-16 <pssarvna.kumr@gmail.com>
2021-03-06 12:38:42 +0530

    Created Connect to Server Button
     Connect to Server : when Clicked it Connects to the server and revert position of all disabled Widgets

--- Client.py
+++ Client.py
@@ -1,6 +1,5 @@
 import socket
 from tkinter import *
-import time
 
 class Socket:
 	ip = ""
@@ -11,6 +10,7 @@ soc = Socket()
 soc.ip = '192.168.43.179'
 soc.port = 8888
 con = ""
+
 def sendData(msg,soc):
 	soc.send(bytes(msg,'utf-8'))
 	print(soc.recv(1024).decode())
@@ -27,26 +27,33 @@ def connectServer(ip,port):
 	print("Led Blinking Code\n");
 
 	msg = soc.con.recv(1024).decode()
-	print(msg)
-	
+	print(msg + "\n")
 	if(msg == "Arduino Not Conneced"):
 		exit()
+	slider.config(state = NORMAL)
+	update_b.config(state = NORMAL)
+	exit_b.config(state = NORMAL)
+	term_b.config(state = NORMAL)
+	connect_b.configure(text = "Connected")
+	return
 
 window = Tk()
 window.title("Remote Lab")
-window.geometry("250x150")
-slider_val = 0
-slider = Scale(window, variable = slider_val, from_ = 0, to = 100, orient = HORIZONTAL)
+window.geometry("250x250")
+
+connect_b = Button(window, text ="Connect to Server", command = lambda:connectServer(soc.ip,soc.port))
+connect_b.pack(pady =10)
+
+slider = Scale(window,from_ = 0, to = 100,state = DISABLED, orient = HORIZONTAL)
 slider.pack(pady =10 )
 
-connectServer(soc.ip,soc.port)
-
-update_b = Button(window, text ="Update Changes", command = lambda:sendData(str(float(slider.get())),soc.con))
+update_b = Button(window, text ="Update Changes",state = DISABLED, command = lambda:sendData(str(float(slider.get())),soc.con))
 update_b.pack(pady =10 )
 
-exit_b = Button(window, text ="Exit", command = lambda:sendData("EXIT",soc.con))
+exit_b = Button(window, text ="Exit", command = lambda:sendData("EXIT",soc.con),state = DISABLED)
 exit_b.pack(padx =20 , side = LEFT)
 
-term_b = Button(window, text ="Terminate", command = lambda:sendData("TERMINATE",soc.con))
+term_b = Button(window, text ="Terminate", command = lambda:sendData("TERMINATE",soc.con),state = DISABLED)
 term_b.pack(padx =20 ,side = RIGHT)
+
 window.mainloop()
