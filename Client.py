1a6b8c8b0d1190169cee30d688159c307447944d
PsSarvna-16 <pssarvna.kumr@gmail.com>
2021-03-06 12:04:25 +0530

    Class Created for Socket

--- Client.py
+++ Client.py
@@ -2,6 +2,15 @@ import socket
 from tkinter import *
 import time
 
+class Socket:
+	ip = ""
+	port = 0
+	con = ""
+
+soc = Socket()
+soc.ip = '192.168.43.179'
+soc.port = 8888
+con = ""
 def sendData(msg,soc):
 	soc.send(bytes(msg,'utf-8'))
 	print(soc.recv(1024).decode())
@@ -11,23 +20,17 @@ def sendData(msg,soc):
 
 def connectServer(ip,port):
 	print("********CLIENT SIDE**********")
-	soc = socket.socket()
+	soc.con = socket.socket()
 
-	soc.connect((ip,port))
+	soc.con.connect((soc.ip,soc.port))
 	print("Connected to the Server\n");
 	print("Led Blinking Code\n");
 
-	msg = soc.recv(1024).decode()
+	msg = soc.con.recv(1024).decode()
 	print(msg)
 	
 	if(msg == "Arduino Not Conneced"):
 		exit()
-	return soc
-
-
-ip = '192.168.43.179'
-port = 8888
-
 
 window = Tk()
 window.title("Remote Lab")
@@ -36,14 +39,14 @@ slider_val = 0
 slider = Scale(window, variable = slider_val, from_ = 0, to = 100, orient = HORIZONTAL)
 slider.pack(pady =10 )
 
-soc = connectServer(ip,port)
+connectServer(soc.ip,soc.port)
 
-update_b = Button(window, text ="Update Changes", command = lambda:sendData(str(float(slider.get())),soc))
+update_b = Button(window, text ="Update Changes", command = lambda:sendData(str(float(slider.get())),soc.con))
 update_b.pack(pady =10 )
 
-exit_b = Button(window, text ="Exit", command = lambda:sendData("EXIT",soc))
+exit_b = Button(window, text ="Exit", command = lambda:sendData("EXIT",soc.con))
 exit_b.pack(padx =20 , side = LEFT)
 
-term_b = Button(window, text ="Terminate", command = lambda:sendData("TERMINATE",soc))
+term_b = Button(window, text ="Terminate", command = lambda:sendData("TERMINATE",soc.con))
 term_b.pack(padx =20 ,side = RIGHT)
 window.mainloop()
